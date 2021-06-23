81b938bb481889a9cd225282f04c4910f9050417
Steven Kaufman <steveplaysguiola@gmail.com>
2021-06-23 12:59:44 -0500

    episode 3

--- backend/ui/http.go
+++ backend/ui/http.go
@@ -9,13 +9,36 @@ type HTTPServer struct {
 	service Service
 }
 
-func NewHTTP(service Service) *HTTPServer {
-	return &HTTPServer{
-		service: service,
-	}
+func NewHTTP() *HTTPServer {
+	return &HTTPServer{}
+}
+
+func (server *HTTPServer) UseService(service Service) {
+	server.service = service
 }
 
 func (server HTTPServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	todos, _ := server.service.GetAllTodos()
+	verified := server.verifyRequest(w, r)
+	if !verified {
+		return
+	}
+
+	todos, err := server.service.GetAllTodos()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 	json.NewEncoder(w).Encode(todos)
+}
+
+func (HTTPServer) verifyRequest(w http.ResponseWriter, r *http.Request) bool {
+	if r.URL.Path != "/todos/" && r.URL.Path != "/todos" {
+		w.WriteHeader(http.StatusNotFound)
+		return false
+	}
+	if r.Method != http.MethodGet {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return false
+	}
+	return true
 }
--- backend/ui/http_test.go
+++ backend/ui/http_test.go
@@ -2,6 +2,7 @@ package ui_test
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -11,9 +12,11 @@ import (
 	"github.com/steve-kaufman/todo-app-tut/backend/ui"
 )
 
+// MockService
+
 type MockService struct {
-	todos []entities.Todo
 	err   error
+	todos []entities.Todo
 }
 
 func (s MockService) GetAllTodos() ([]entities.Todo, error) {
@@ -23,39 +26,144 @@ func (s MockService) GetAllTodos() ([]entities.Todo, error) {
 	return s.todos, nil
 }
 
-func TestHTTP(t *testing.T) {
-	var expect expectate.ExpectorFunc
-	var server *ui.HTTPServer
-	var w *httptest.ResponseRecorder
-	var r *http.Request
+// dummyTodos
 
-	setup := func(t *testing.T, service *MockService) {
-		expect = expectate.Expect(t)
-		server = ui.NewHTTP(service)
-		w = httptest.NewRecorder()
-		r = httptest.NewRequest(http.MethodGet, "http://mywebsite.com/", nil)
+var dummyTodos = []entities.Todo{
+	{
+		Title:       "todo 1",
+		Description: "description of todo 1",
+		IsCompleted: true,
+	},
+	{
+		Title:       "todo 2",
+		Description: "description of todo 2",
+		IsCompleted: false,
+	},
+	{
+		Title:       "todo 3",
+		Description: "description of todo 3",
+		IsCompleted: true,
+	},
+}
+
+// HTTPTest
+
+type HTTPTest struct {
+	name string
+
+	service     *MockService
+	inputMethod string
+	inputURL    string
+
+	expectedStatus int
+	expectedTodos  []entities.Todo
+}
+
+func TestHTTP(t *testing.T) {
+	tests := getTests()
+	tests = append(tests, getDisallowedMethodTests()...)
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			testHTTP(t, test)
+		})
+	}
+}
+
+// Main Test
+
+func testHTTP(t *testing.T, test HTTPTest) {
+	expect := expectate.Expect(t)
+
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest(test.inputMethod, test.inputURL, nil)
+
+	server := ui.NewHTTP()
+	server.UseService(test.service)
+
+	server.ServeHTTP(w, r)
+
+	var body []entities.Todo
+	json.NewDecoder(w.Result().Body).Decode(&body)
+
+	expect(w.Result().StatusCode).ToBe(test.expectedStatus)
+	expect(body).ToEqual(test.expectedTodos)
+}
+
+func getTests() []HTTPTest {
+	return []HTTPTest{
+		{
+			name: "Random error gives 500 status and no todos",
+
+			service:     &MockService{err: fmt.Errorf("something bad happened")},
+			inputMethod: "GET",
+			inputURL:    "http://mywebsite.com/todos/",
+
+			expectedStatus: 500,
+		},
+		{
+			name: "Random error gives 500 status and no todos",
+
+			service:     &MockService{err: fmt.Errorf("something bad happened")},
+			inputMethod: "GET",
+			inputURL:    "http://mywebsite.com/todos",
+
+			expectedStatus: 500,
+		},
+		{
+			name: "Wrong path gives 404 status and no todos",
+
+			service:     &MockService{todos: dummyTodos},
+			inputMethod: "GET",
+			inputURL:    "http://mywebsite.com/foo",
+
+			expectedStatus: 404,
+		},
+		{
+			name: "Wrong path gives 404 status and no todos",
+
+			service:     &MockService{todos: dummyTodos},
+			inputMethod: "GET",
+			inputURL:    "http://mywebsite.com/bar",
+
+			expectedStatus: 404,
+		},
+		{
+			name: "Returns todos from service if no error",
+
+			service:     &MockService{todos: dummyTodos},
+			inputMethod: "GET",
+			inputURL:    "http://mywebsite.com/todos/",
+
+			expectedStatus: 200,
+			expectedTodos:  dummyTodos,
+		},
+	}
+}
+
+func getDisallowedMethodTests() []HTTPTest {
+	tests := []HTTPTest{}
+
+	disallowedMethods := []string{
+		http.MethodDelete,
+		http.MethodHead,
+		http.MethodOptions,
+		http.MethodPatch,
+		http.MethodPost,
+		http.MethodPut,
 	}
 
-	t.Run("Returns todos from root endpoint", func(t *testing.T) {
-		setup(t, &MockService{todos: []entities.Todo{
-			{
-				Title:       "mock todo",
-				Description: "description of mock todo",
-				IsCompleted: false,
-			},
-		}})
+	for _, method := range disallowedMethods {
+		tests = append(tests, HTTPTest{
+			name: fmt.Sprintf("Method %s gives 405 status and no todos", method),
 
-		server.ServeHTTP(w, r)
+			service:     &MockService{todos: dummyTodos},
+			inputURL:    "http://mywebsite.com/todos/",
+			inputMethod: method,
 
-		var body []entities.Todo
-		json.NewDecoder(w.Result().Body).Decode(&body)
-
-		expect(body).ToEqual([]entities.Todo{
-			{
-				Title:       "mock todo",
-				Description: "description of mock todo",
-				IsCompleted: false,
-			},
+			expectedStatus: http.StatusMethodNotAllowed,
 		})
-	})
+	}
+
+	return tests
 }
